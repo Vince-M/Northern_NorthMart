4d85a9b68846896cdeca841e82d7a81d8f2fa450
Vince Mendella <vmendella@gmail.com>
2024-12-03 16:23:46 -0600

    add front-page - make default homepage. Add blog page

--- index.php
+++ index.php
@@ -2,126 +2,27 @@
 
         <!-- HERO
         =================================================== -->
-        <section class="full-width-split-screen hero__section--index">
-          <div class="hero__section--intro">
-            <h1 class="hero__index">
-              Your trusted community store.
-            </h1>
+        <section class="hero__section hero__section--about full-width-split-screen">
+          
+          <div class="hero__text">
+            <h1 class="hero__head">Welcome to Northern/NorthMart</h1>
+            <h2 class="hero__subhead"></h2>
           </div>
-          <img class="hero__section--image" src="img/ncr_hero_1600x944.jpg" alt="">
-
+          <img class="hero__section--image" src="img/hero_about2_1264x896.webp" alt="">
         </section>
         <!-- ============================================== -->
 
-        <!-- PROMO BOXES
+          <!-- CONTENT
         =================================================== -->
-        <section class="wrapper promo__boxes">
-          <div class="col1 fade-in">
-            <img src="img/promobox-hp_pampers_1280x640.jpg" alt="Pampers Diapers, Selected Varieties" width="640" height="320">
-          </div>
-          <div class="col2 fade-in">
-            <img src="img/promobox-hp_bingo_1280x640.jpg" alt="Promotional Bingo Contest" width="640" height="320">
-          </div>
-        </section>
-        <!-- ============================================== -->
-
-        <!-- SAVING PROMO
-        =================================================== -->
-        <section class="inStore__promo">
-          <div class="wrapper priceDropBanner">
-            <img class="pdBanner" src="img/priceDrop_banner.svg" alt="">
-            <img  class="saveMore" src="img/priceDrop_helpingYouSaveMore.svg" alt="">
-          </div>
-        </section>
-        <!-- ============================================== -->
-
-        <!-- FULL WIDTH PROMO BOXES
-        =================================================== -->
-        <section class="wrapper promo__boxes--fw">
-          <div class="fade-in fullWidth">
-            <img src="img/fw_vendor_branded-jackets_1240x320.webp" alt="" width="1240" height="320">
-          </div>
-        </section>
-        <!-- ============================================== -->
-
-        <!-- RECIPE
-        =================================================== -->
-        <section class="wrapper indexRecipes">
-          <div class="indexRecipe__image">
-            <img src="img/recipe_koreanBeef_1232x400.jpg" alt="plate of Korean Beef with Brocolli and Beef with Sesame Seeds" width="1280" height="400">
-          </div>
-          <div class="indexRecipe__textBlock">
-            <p class="indexRecipe__head">recipe</p>
-            <h2 class="indexRecipe__title">Korean Beef and Broccoli</h2>
-            <p class="indexRecipe__description">Serve this saucy meat dish over rice and broccoli.</p>
-            <p class="indexRecipe__link"><a href="#">view recipe &#10095;</a></p>
-          </div>
-        </section>
-        <!-- ============================================== -->
-
-        <!-- PROMO BOXES
-        =================================================== -->
-        <section class="wrapper promo__boxes">
-          <div class="col1 fade-in">
-            <img src="img/promobox-hp_northy_1280x640.jpg" alt="Promotional Northy the Bear" width="640" height="320">
-          </div>
-          <div class="col2 fade-in">
-            <a href="landingPge.html"><img src="img/promobox-hp_activia_1280x640.jpg" alt="Promotional Bingo Contest" width="640" height="320"></a>
-          </div>
-        </section>
-        <!-- ============================================== -->
-
-        <!-- ADVOCACY
-        =================================================== -->
-        <section class="advocacy full-width-split-screen">
-          <!-- <figure class="advocacy__index--img">
-            <img src="img/index_advocay_960x480.jpg" alt="Advocacy and Awareness" width="960" height="480">
-          </figure> -->
-
-          <img src="img/index_advocay_960x480.jpg" alt="Advocacy and Awareness" width="960" height="480">
-
-          <div class="advocacy__text">
-            <h2>Advocacy and Awareness</h2>
-            <h3>Orange Shirt Day</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
-            <div class="btn btn__advocacy">
-              Learn More
+        <div class="wrapper">
+          <div class="full-width-split-screen content__intro subpage__ncrCol1">
+            <div class="textIntro subpage__textIntro">
+              <p>Look for the Price Drop signs throughout the store - you'll find items specially selected to help you save. From staples like meats, fruits and vegetables to family favourites like pizza, cheese slices and chicken wings, all our Price Drop items have been chosen to help you fill your basket for less. And all Price Drop items are locked in for a 4 or 5 week period to help you save even more!</p>
             </div>
+
           </div>
 
-        </section>
-        <!-- ============================================== -->
-
-        <!-- SITE CARDS
-        =================================================== -->
-        <section class="wrapper site__cards">
-          <div class="col1">
-            <img class="site__img" src="img/cards-hhf_1280x672.jpg" alt="Healthy Horizons Foundation" width="640" height="336">
-            <div class="site__text">
-              <p><span class="site__text--callout">The Healthy Horizons Foundation</span> is dedicated to improving the health and well-being of youth in the communities we serve through financial support and the motivation to advance their level of fitness, education, and health.</p>
-
-              <p>Your generosity will play an important role in our efforts to give youth in our communities a brighter and healthier future! <a class="hhf__textLink" href="healthyHorizons.html">[ View HHF ]</a> </p>
-              <a href="healthyHorizons.html"><div class="btn btn__primary">
-                HHF Learn More
-              </div></a>
-            </div>
-          </div>
-          <div class="col2">
-            <img class="site__img"  src="img/cards-weFinancial_1280x672.jpg" alt="We Financial" width="640" height="336">
-            <div class="site__text">
-              <p>What does <span class="site__text--callout">We</span> mean? It's so simple, and yet it means so much. <span class="site__text--callout">We</span> reflects our dedication and connection to the community, has a sense of shared ownership with the customer, and is friendly and human.</p>
-
-              <ul class="we__cardList">
-                <li>You can use it for online shopping, and reserving airline tickets and hotels.</li>
-                <li>You can sign up for email alerts to notify you when your card has been loaded or when your balance is getting low.</li>
-                <li>You can get your balance online for free.</li>
-              </ul>
-              <a href="weFinancial.html"><div class="btn btn__we">
-                Visit We Financial
-              </div></a>
-            </div>
-          </div>
-        </section>
+        </div>
         <!-- ============================================== -->
 
 <?php get_footer(); ?>
