0f67187f4d0f65f56a7d82e1b4c9b4bec7ac490a
Vince Mendella <vmendella@gmail.com>
2024-12-05 16:07:30 -0600

    add page templates for Community and HHF

---
+++ page-hhf.php
@@ -0,0 +1,41 @@
+<?php
+/*
+  Template Name: HHF template
+*/
+?>
+
+<?php get_header(); ?>
+
+    <!-- MAIN
+    =================================================== -->
+    <main id="main" class="site-content main" role="main">
+    <article>
+
+      <!-- HERO
+      =================================================== -->
+      <section class="hero__section hero__section--healthyHorizons full-width-split-screen">
+        
+        <div class="hero__text">
+          <h1 class="hero__head">
+            <?php echo get_field('page_hero_heading' ); ?>
+          </h1>
+          <h2 class="hero__subhead">
+          <?php echo get_field('page_hero_subheading' ); ?>
+          </h2>
+          <!-- <h1 class="hero__head">Community</h1> -->
+          <!-- <h2 class="hero__subhead">Making a difference in the communities we serve</h2> -->
+        </div>
+        <?php 
+          $image = get_field('page_hero_image');
+          if( !empty( $image ) ): ?>
+              <img class="page_hero_image" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" title="<?php echo esc_attr($image['title']); ?>" />
+        <?php endif; ?>
+        <!-- <img class="hero__section--image" src="img/hero_community_1264x896.jpg" alt=""> -->
+      </section>
+      <!-- ============================================== -->
+
+    </article>
+    </main>
+
+
+<?php get_footer(); ?>
